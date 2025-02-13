801b677129c26c30c0c76238b6a8e6a34ada2994
ksewina <17175236+ksewina@users.noreply.github.com>
2025-02-13 22:42:52 +0100

    format on save, toykonight colorscheme

--- lvim/.config/lvim/lua/ks/options.lua
+++ lvim/.config/lvim/lua/ks/options.lua
@@ -1,3 +1,6 @@
+lvim.colorscheme = "tokyonight"
+
+
 local opt = vim.opt -- for conciseness
 
 opt.autowrite = true
@@ -5,14 +8,14 @@ opt.autowrite = true
 -- line numbers
 
 opt.relativenumber = true -- show relative line numbers
-opt.number = true -- shows absolute line number on cursor line (when relative number is on)
+opt.number = true         -- shows absolute line number on cursor line (when relative number is on)
 
 -- tabs & indentation
 
 opt.tabstop = 2
 opt.softtabstop = 2
-opt.shiftwidth = 2 -- amount of characters are used to indent with >> <<
-opt.expandtab = true -- expand tab to spaces
+opt.shiftwidth = 2    -- amount of characters are used to indent with >> <<
+opt.expandtab = true  -- expand tab to spaces
 opt.autoindent = true -- copy indent from current line when starting new one
 
 -- line wrapping
@@ -24,4 +27,4 @@ opt.wrap = false -- disable line wrapping
 opt.termguicolors = true
 -- opt.t_Co=256 for ubuntu terminal
 
-
+lvim.format_on_save = true
